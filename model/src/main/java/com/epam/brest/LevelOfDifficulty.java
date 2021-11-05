7844142b488bd6fd194dd8dd3d88227322b3c3e2
Alex Remizov <rem.remizov@yandex.ru>
2021-11-05 18:31:04 +0300

    Add model module

---
+++ model/src/main/java/com/epam/brest/LevelOfDifficulty.java
@@ -0,0 +1,5 @@
+package com.epam.brest;
+
+public enum LevelOfDifficulty {
+    EASY,MEDIUM,HARD
+}
---
+++ model/src/main/java/com/epam/brest/Order.java
@@ -0,0 +1,22 @@
+package com.epam.brest;
+
+public class Order {
+    private Integer orderId;
+    private String address;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}

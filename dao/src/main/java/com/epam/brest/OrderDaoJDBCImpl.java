731b459bcfc7f65ba8e8568047b61c92caa24276
Alex Remizov <rem.remizov@yandex.ru>
2021-11-08 14:10:28 +0300

    Add OrdeDaoJDBCImpl

---
+++ dao/src/main/java/com/epam/brest/OrderDaoJDBCImpl.java
@@ -0,0 +1,25 @@
+package com.epam.brest;
+
+import java.util.List;
+
+public class OrderDaoJDBCImpl implements OrderDao{
+    @Override
+    public List<Order> findAll() {
+        return null;
+    }
+
+    @Override
+    public Integer create(Order order) {
+        return null;
+    }
+
+    @Override
+    public Integer update(Order order) {
+        return null;
+    }
+
+    @Override
+    public Integer delete(Integer orderId) {
+        return null;
+    }
+}

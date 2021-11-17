8da32213294317eb543ed7a249a9b2755fdabdb4
Alex Remizov <rem.remizov@yandex.ru>
2021-11-17 12:54:31 +0300

    Fix documentation

--- model/src/main/java/com/epam/brest/Client.java
+++ model/src/main/java/com/epam/brest/Client.java
@@ -1,8 +1,13 @@
 package com.epam.brest;
 
+import java.util.Objects;
+
 public class Client {
     private Integer clientId;
     private String clientName;
+
+    public Client(){
+    }
 
     public Client(String clientName) {
         this.clientName = clientName;
@@ -22,5 +27,27 @@ public class Client {
 
     public void setClientName(String clientName) {
         this.clientName = clientName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if ((!(o instanceof Client))) return false;
+        Client client = (Client) o;
+        return Objects.equals(getClientId(), client.getClientId()) &&
+                Objects.equals(getClientName(), client.getClientName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getClientId(), getClientName());
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "clientId=" + clientId +
+                ", clientName='" + clientName + '\'' +
+                '}';
     }
 }
--- model/src/main/java/com/epam/brest/Repair.java
+++ model/src/main/java/com/epam/brest/Repair.java
@@ -1,7 +1,7 @@
 package com.epam.brest;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Repair {
     private Integer repairId;
@@ -9,10 +9,17 @@ public class Repair {
     private String address;
     private LevelOfDifficulty difficultyLevel;
     private LocalDate preferenceDate;
-    private Integer amountOfCrew;
-    private BigDecimal repairPrice;
     private Integer clientId;
 
+    public Repair(){
+    }
+
+    public Repair(RepairType repairType, String address, LevelOfDifficulty difficultyLevel, LocalDate preferenceDate) {
+        this.repairType = repairType;
+        this.address = address;
+        this.difficultyLevel = difficultyLevel;
+        this.preferenceDate = preferenceDate;
+    }
 
     public Integer getRepairId() {
         return repairId;
@@ -54,22 +61,6 @@ public class Repair {
         this.preferenceDate = preferenceDate;
     }
 
-    public Integer getAmountOfCrew() {
-        return amountOfCrew;
-    }
-
-    public void setAmountOfCrew(Integer amountOfCrew) {
-        this.amountOfCrew = amountOfCrew;
-    }
-
-    public BigDecimal getRepairPrice() {
-        return repairPrice;
-    }
-
-    public void setRepairPrice(BigDecimal repairPrice) {
-        this.repairPrice = repairPrice;
-    }
-
     public Integer getClientId() {
         return clientId;
     }
@@ -78,4 +69,32 @@ public class Repair {
         this.clientId = clientId;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Repair)) return false;
+        Repair repair = (Repair) o;
+        return Objects.equals(getRepairId(), repair.getRepairId()) &&
+                getRepairType() == repair.getRepairType() && Objects.equals(getAddress(), repair.getAddress()) &&
+                getDifficultyLevel() == repair.getDifficultyLevel() &&
+                Objects.equals(getPreferenceDate(), repair.getPreferenceDate()) &&
+                Objects.equals(getClientId(), repair.getClientId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRepairId(), getRepairType(), getAddress(), getDifficultyLevel(), getPreferenceDate(), getClientId());
+    }
+
+    @Override
+    public String toString() {
+        return "Repair{" +
+                "repairId=" + repairId +
+                ", repairType=" + repairType +
+                ", address='" + address + '\'' +
+                ", difficultyLevel=" + difficultyLevel +
+                ", preferenceDate=" + preferenceDate +
+                ", clientId=" + clientId +
+                '}';
+    }
 }

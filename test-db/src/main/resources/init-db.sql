29542a30b5da871b97e1ee499fbf7733018a66ca
Alex Remizov <rem.remizov@yandex.ru>
2021-11-18 23:21:47 +0300

    Fix init db

--- test-db/src/main/resources/init-db.sql
+++ test-db/src/main/resources/init-db.sql
@@ -1,3 +1,3 @@
-INSERT INTO client(client_id, client_name) VALUES (1, "Aleksandrovich Aleksey Iosifovich");
-INSERT INTO client(orders_id, client_name) VALUES (2, "Orlov Petr Ivanovich");
-INSERT INTO client(orders_id, client_name) VALUES (3, "Borodach Michail Ivanovich");
+INSERT INTO client(client_id, client_name) VALUES (1, 'Aleksandrovich Aleksey Iosifovich');
+INSERT INTO client(client_id, client_name) VALUES (2, 'Orlov Petr Ivanovich');
+INSERT INTO client(client_id, client_name) VALUES (3, 'Borodach Michail Ivanovich');

36be43bb1b71d259084dd0d9333f6e562161bf23
Alex Remizov <rem.remizov@yandex.ru>
2021-11-25 19:17:46 +0300

    Add method create in ClientDaoJDBCImpl

--- dao/src/test/java/com/epam/brest/dao/ClientDaoJDBCImplIT.java
+++ dao/src/test/java/com/epam/brest/dao/ClientDaoJDBCImplIT.java
@@ -6,13 +6,16 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DuplicateKeyException;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-jdbc-conf.xml"})
+@Transactional
+@Rollback
 class ClientDaoJDBCImplIT {
     private final Logger log = LogManager.getLogger(ClientDaoJDBCImplIT.class);
 
@@ -37,14 +40,14 @@ class ClientDaoJDBCImplIT {
         int newDepartmentId = clientDaoJDBC.create(client);
         assertNotNull(newDepartmentId);
         assertEquals((int) clientSizeBefore, clientDaoJDBC.findAll().size() - 1);
-
     }
+
     @Test
     void tryToCreateEqualsClient() {
         log.debug("Execute test: tryToCreateEqualsClient()");
         assertNotNull(clientDaoJDBC);
-        Client client = new Client("Olenev Grigoriy Ivanovich");
-        assertThrows(DuplicateKeyException.class, () -> {
+        Client client = new Client("Borisuk Oleg Aleksandrovich");
+        assertThrows(IllegalArgumentException.class, () -> {
             clientDaoJDBC.create(client);
             clientDaoJDBC.create(client);
         });

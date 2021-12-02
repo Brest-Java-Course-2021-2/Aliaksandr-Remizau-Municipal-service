6d018a5ac91a5fd114543b6e4faf6c5977b78f3c
Alex Remizov <rem.remizov@yandex.ru>
2021-12-03 00:35:42 +0300

    Add VersionControllerTest

---
+++ rest/src/test/java/com/epam/brest/rest/VersionControllerTest.java
@@ -0,0 +1,14 @@
+package com.epam.brest.rest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VersionControllerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getVersion() {
+    }
+}
